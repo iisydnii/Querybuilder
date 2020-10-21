81561e305793d63c6c97eb8e39736a767745e774
iisydnii <61031342+iisydnii@users.noreply.github.com>
2020-10-20 22:09:50 -0400

    update!

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 
 namespace Lab5
 {
@@ -7,15 +9,32 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            List<Dictionary<String, List<String>>> singleRow = new List<Dictionary<String, List<String>>>();
-            List<string> list = new List<string>();
+            List<string> listforKeys = new List<string>();
+            List<string> listforValues = new List<string>();
+            List<string> update = new List<string>();
+            Dictionary<List<string>, List<string>> create = new Dictionary<List<string>, List<string>>();
+            string dbSource = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+            Console.WriteLine(dbSource);
+            
+            
             QueryBuilder queryBuilder = new QueryBuilder();
-            queryBuilder.ReadAll("Student");
-            list.Add("Sydni");
-            list.Add("Ward");
-            singleRow.Add(new Dictionary<string, List<string>>() { { "1", list } });
-            queryBuilder.Create("Student", singleRow);
+            //listforKeys.Add("Abbreviation");
+            //listforKeys.Add("Name");
 
+            //listforValues.Add("CSCI");
+            //listforValues.Add("Computer Science");
+
+            //create.Add(listforKeys, listforValues);
+            using (var connection = new SqliteConnection($"Data Source ={dbSource}/Database/myDatabase.db"))
+            {
+                connection.Open();
+                queryBuilder.Read(connection, "Student", "1");
+                queryBuilder.Read(connection, "Course", "1");
+                //queryBuilder.Create(connection, "Major", create);
+                queryBuilder.Update(connection, "Student", 2, "FirstName = 'Sydni'");
+                queryBuilder.Update(connection, "Student", 2, "LastName = 'Ward'");
+                queryBuilder.Delete(connection, "Student", 5);
+            }
 
         }
     }
--- Lab5/QueryBuilder.cs
+++ Lab5/QueryBuilder.cs
@@ -1,226 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Data.Sqlite;
 
 namespace Lab5
 {
     public class QueryBuilder
     {
-        List<Dictionary<String, List<String>>> database;
-        List<Dictionary<String, List<String>>> singleRow;
-        string key;
-        int row;
-
+        
+        List<string> list = new List<string>();
+        List<Dictionary<int, List<String>>> database;
+        List<Dictionary<int, List<String>>> selectiveQuery;
+        
 
         public QueryBuilder()
         {
-            database = new List<Dictionary<String, List<String>>>();
-            singleRow = new List<Dictionary<String, List<String>>>();
-            SQLiteConnection = @"Data Source= myDatabase.db";
+            database = new List<Dictionary<int, List<String>>>();
+            selectiveQuery = new List<Dictionary<int, List<String>>>();
+            this.SQLiteConnection = SQLiteConnection;
         }
 
         public string SQLiteConnection { get; set;}
 
-        public List<Dictionary<String, List<String>>> ReadAll(string tableName)
+        public List<Dictionary<int, List<String>>> ReadAll(SqliteConnection connection, string tableName)
         {
-            using (var connection = new SqliteConnection(SQLiteConnection))
-            {
-                connection.Open();
                 var command = connection.CreateCommand();
-                command.CommandText = "select * from " + tableName;
+                command.CommandText = $"select * from {tableName}";
                 SqliteDataReader reader = command.ExecuteReader();
 
                 if (reader.HasRows)
                 {
-                    int index = 0;
                     while (reader.Read())
                     {
-                        List<string> list = new List<string>();
-                        switch (tableName)
+                        
+                        for (int i = 0; i < reader.FieldCount; i++)
                         {
-                            case "Course":
-                                list.Add(reader["MajorId"].ToString());
-                                list.Add(reader["CourseNumber"].ToString());
-                                list.Add(reader["Name"].ToString());
-                                list.Add(reader["CreditHour"].ToString());
-                                break;
-                            case "Major":
-                                list.Add(reader["Abbreviation"].ToString());
-                                list.Add(reader["Name"].ToString());
-                                break;
-                            case "sqlite_sequence":
-                                list.Add(reader["seq"].ToString());
-                                break;
-                            case "Student":
-                                list.Add(reader["FirstName"].ToString());
-                                list.Add(reader["LastName"].ToString());
-                                break;
-                            default:
-                                Console.WriteLine("Error!");
-                                break;
+                            list.Add(reader.GetName(i) + ": " + reader.GetValue(i));
+                            database.Add(new Dictionary<int, List<String>>()
+                            {{ i , list}});
                         }
-
-                        database.Add(new Dictionary<String, List<String>>()
-                        {{reader[index].ToString() , list}});
                     }
                 }
                 else
                 {
                     Console.WriteLine("No rows found.");
                 }
+            database.ToString();
                 return database;
-            }
         }
 
-        public List<Dictionary<String, List<String>>> Read(string tableName, string primaryKey)
+        public List<Dictionary<int, List<String>>> Read(SqliteConnection connection, string tableName, string key)
         {
-            using (var connection = new SqliteConnection(SQLiteConnection))
-            {
-                connection.Open();
-                GetKey(tableName);
-                var command = connection.CreateCommand();
-                command.CommandText = "select * from " + tableName +
-                        "where " + key + " = " + primaryKey;
-                SqliteDataReader reader = command.ExecuteReader();
+            var command = connection.CreateCommand();
+            command.CommandText = $"select * from {tableName} where id = {key}";
+            SqliteDataReader reader = command.ExecuteReader();
 
-                if (reader.HasRows)
+            if (reader.HasRows)
+            {
+                while (reader.Read())
                 {
-                    int index = 0;
-                    while (reader.Read())
+
+                    for (int i = 0; i < reader.FieldCount; i++)
                     {
-                        List<string> list = new List<string>();
-                        switch (tableName)
-                        {
-                            case "Course":
-                                list.Add(reader["MajorId"].ToString());
-                                list.Add(reader["CourseNumber"].ToString());
-                                list.Add(reader["Name"].ToString());
-                                list.Add(reader["CreditHour"].ToString());
-                                break;
-                            case "Major":
-                                list.Add(reader["Abbreviation"].ToString());
-                                list.Add(reader["Name"].ToString());
-                                break;
-                            case "sqlite_sequence":
-                                list.Add(reader["seq"].ToString());
-                                break;
-                            case "Student":
-                                list.Add(reader["FirstName"].ToString());
-                                list.Add(reader["LastName"].ToString());
-                                break;
-                            default:
-                                Console.WriteLine("Error!");
-                                break;
-                        }
-                        singleRow.Add(new Dictionary<String, List<String>>()
-                        {{reader[index].ToString() , list}});
+                        list.Add(reader.GetName(i) + ": " + reader.GetValue(i));
+                        selectiveQuery.Add(new Dictionary<int, List<String>>()
+                            {{ i , list}});
                     }
                 }
-                else
-                {
-                    Console.WriteLine("No rows found.");
-                }
-                return singleRow;
             }
-        }
-
-        public void Create(string tableName, List<Dictionary<String, List<String>>> create)
-        {
-            using (var connection = new SqliteConnection(SQLiteConnection))
+            else
             {
-                connection.Open();
-                GetKey(tableName);
-                var command = connection.CreateCommand();
-                // PROBLEM With the AUTO-INCREMENTING ID
-                //command.CommandText = "select count(Id) from " + tableName;
-                //SqliteDataReader reader = command.ExecuteReader();
-
-                //if (reader.HasRows)
-                //{
-                //    int index = 0;
-                //    while (reader.Read())
-                //    {
-                //        key = reader[index].ToString();
-                //        row = Int32.Parse(key) + 1;
-                //    }
-                //}
-                //// PROBLEM With the AUTO-INCREMENTING ID
-                switch (tableName)
-                {
-                    case "Course":
-                        command.CommandText = "INSERT INTO " + tableName +
-                        " (Id, MajorId, CourseNumber, Name, CreditHour) VALUES ("
-                        + row.ToString() + create[1] + create[2] + create[3] + create[4]
-                         + ")";
-                        command.Parameters.AddWithValue("Id", row.ToString());
-                        command.Parameters.AddWithValue("MajorId", create[1]);
-                        command.Parameters.AddWithValue("CourseNumber", create[2]);
-                        command.Parameters.AddWithValue("Name", create[3]);
-                        command.Parameters.AddWithValue("CreditHour", create[4]);
-                        break;
-                    case "Major":
-                        command.CommandText = "INSERT INTO " + tableName +
-                    " (Id, Abbreviation, Name) VALUES (" + row.ToString()
-                    + create[1] + create[2] + ")";
-                        command.Parameters.AddWithValue("Id", row.ToString());
-                        command.Parameters.AddWithValue("Abbreviation", create[1]);
-                        command.Parameters.AddWithValue("Name", create[2]);
-                        break;
-                    case "sqlite_sequence":
-                        command.CommandText = "INSERT INTO " + tableName +
-                    " (name, seq) VALUES (" + create[0] + create[1]
-                     + ")";
-                        command.Parameters.AddWithValue("name", create[0]);
-                        command.Parameters.AddWithValue("seg", create[1]);
-                        break;
-                    case "Student":
-                        command.CommandText = "INSERT INTO " + tableName +
-                    " (Id, FirstName, LastName) VALUES (" + row.ToString()
-                    + create[1] + create[2] + ")";
-                        command.Parameters.AddWithValue("Id", row.ToString());
-                        command.Parameters.AddWithValue("FirstName", create[1]);
-                        command.Parameters.AddWithValue("LastName", create[2]);
-                        break;
-                    default:
-                        Console.WriteLine("Can't find table Error!");
-                        break;
-                }
-
-
+                Console.WriteLine("No rows found.");
             }
+            return selectiveQuery;
         }
 
-        public void Update()
-        {
-        }
-        public void Delete()
-        {
-        }
-        private string GetKey(String tableName)
-        {
-            switch (tableName)
-            {
-                case "Course":
-                    key = "Id";
-                    break;
-                case "Major":
-                    key = "Id";
-                    break;
-                case "sqlite_sequence":
-                    key = "name";
-                    break;
-                case "Student":
-                    key = "Id";
-                    break;
-                default:
-                    key = "";
-                    Console.WriteLine("Can't find table Error!");
-                    break;
-            }
+        //public void Create(SqliteConnection connection, string tableName, Dictionary<List<string>, List<string>> create)
+        //{
+        //    ReadAll(connection, tableName);
+        //    var command = connection.CreateCommand();
+        //    for (int i = 0; i < create.Count())
 
-            return key;
+        //    command.CommandText = $"INSERT INTO {tableName} ( {create.Keys} ) VALUES ( {create.TryGetValue(create.Keys)} )";
+        //    command.ExecuteNonQuery();
+        //    ReadAll(connection, tableName);
+        //}
 
+        public void Update(SqliteConnection connection, string tableName, int key , string update)
+        {
+            ReadAll(connection, tableName);
+            var command = connection.CreateCommand();
+
+            command.CommandText = $"UPDATE {tableName} SET {update} WHERE id = {key} ";
+            command.ExecuteNonQuery();
+            ReadAll(connection, tableName);
         }
+
+        public void Delete(SqliteConnection connection, string tableName, int key)
+        {
+            ReadAll(connection, tableName);
+            var command = connection.CreateCommand();
+            command.CommandText = $"delete from {tableName} where id = {key}";
+            command.ExecuteNonQuery();
+            ReadAll(connection, tableName);
+        }
+
     }
 }
 
